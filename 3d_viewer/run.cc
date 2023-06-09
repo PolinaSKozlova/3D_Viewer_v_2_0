103433f9c4e363340fcf62e0e7b1fb959cad5189
verenach <verenach@student.21-school.ru>
2023-06-09 17:43:52 +0700

    проверка работы афинок

--- 3d_viewer/run.cc
+++ 3d_viewer/run.cc
@@ -1,9 +1,20 @@
-#include "affine_transformations/s21_affine_transformations.h"
-#include "affine_transformations/s21_matrix.h"
+#include "affine_transformations/s21_matrix4X4.h"
 
 int main() {
-  s21::AffineTransformations af;
-  af.MakeMovement();
-  af.print();
+  s21::Matrix4X4 m(4);
+  s21::TransformData transformations;
+  transformations.x_rotation_deg = -12;
+  transformations.y_rotation_deg = 34;
+  transformations.user_scaler = 0;
+  transformations.z_shift = 20;
+  m.SetData(transformations);
+  m.MakeMovement();
+  m.print();
+  transformations.x_rotation_deg = 0;
+  transformations.x_shift = -18;
+  transformations.z_rotation_deg = 10;
+  m.SetData(transformations);
+  m.MakeMovement();
+  m.print();
   return 0;
 }
