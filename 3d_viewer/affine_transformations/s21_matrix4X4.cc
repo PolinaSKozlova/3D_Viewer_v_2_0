9a5d80f0b89416ae8a5b1cb7ce5d027971c02030
verenach <verenach@student.21-school.ru>
2023-06-09 17:45:09 +0700

    удалила лишнее, переименовала файлы

---
+++ 3d_viewer/affine_transformations/s21_matrix4X4.cc
@@ -0,0 +1,104 @@
+#include "s21_matrix4X4.h"
+
+namespace s21 {
+
+Matrix4X4::Matrix4X4() : rows_(4), cols_(4) {
+  MemoryAllocate();
+  // InitializeIdentityMatrix();
+}
+
+Matrix4X4::Matrix4X4(int size) : rows_(size), cols_(size) {
+  if (size <= 0) throw std::invalid_argument("Size can't be less than zero");
+  MemoryAllocate();
+  InitializeIdentityMatrix();
+}
+
+Matrix4X4::Matrix4X4(const Matrix4X4& other) { *this = other; }
+
+Matrix4X4::~Matrix4X4() {
+  if (matrix_) MemoryFree();
+  matrix_ = nullptr;
+  rows_ = cols_ = 0;
+}
+
+int Matrix4X4::GetRows() const noexcept { return rows_; }
+
+int Matrix4X4::GetCols() const noexcept { return cols_; }
+
+void Matrix4X4::MulMatrix(const Matrix4X4& other) { *this *= other; }
+
+Matrix4X4& Matrix4X4::operator=(const Matrix4X4& other) noexcept {
+  if (matrix_ != nullptr) {
+    MemoryFree();
+  }
+  rows_ = other.rows_;
+  cols_ = other.cols_;
+  MemoryAllocate();
+  for (int i = 0; i < other.rows_; ++i) {
+    for (int j = 0; j < other.cols_; ++j) {
+      matrix_[i][j] = other.matrix_[i][j];
+    }
+  }
+  return *this;
+}
+
+Matrix4X4& Matrix4X4::operator*=(const Matrix4X4& other) noexcept {
+  Matrix4X4 result_matrix;
+  for (int i = 0; i < rows_; ++i) {
+    for (int j = 0; j < cols_; ++j) {
+      for (int k = 0; k < cols_; ++k) {
+        result_matrix.matrix_[i][j] += matrix_[i][k] * other.matrix_[k][j];
+      }
+    }
+  }
+  *this = result_matrix;
+  return *this;
+}
+
+Matrix4X4& Matrix4X4::operator*=(const double& num) noexcept {
+  MulNumber(num);
+  return *this;
+}
+
+void Matrix4X4::MulNumber(const double& num) noexcept {
+  for (int i = 0; i < rows_; ++i) {
+    for (int j = 0; j < cols_; ++j) {
+      matrix_[i][j] *= num;
+    }
+  }
+}
+
+void Matrix4X4::SetData(const TransformData& other) {
+  data_.x_rotation_deg = other.x_rotation_deg;
+  data_.y_rotation_deg = other.y_rotation_deg;
+  data_.z_rotation_deg = other.z_rotation_deg;
+  data_.x_shift = other.x_shift;
+  data_.y_shift = other.y_shift;
+  data_.z_shift = other.z_shift;
+  data_.user_scaler = other.user_scaler;
+  data_.model_scaler = other.model_scaler;
+  data_.perspective_ortho = other.perspective_ortho;
+}
+
+void Matrix4X4::MemoryAllocate() {
+  matrix_ = new double*[rows_];
+  for (int i = 0; i < rows_; ++i) {
+    matrix_[i] = new double[cols_]{};
+  }
+}
+
+void Matrix4X4::MemoryFree() {
+  for (int i = 0; i < rows_; ++i) {
+    delete[] matrix_[i];
+  }
+  delete[] matrix_;
+}
+
+void Matrix4X4::InitializeIdentityMatrix() {
+  for (int i = 0; i < rows_; ++i) {
+    for (int j = 0; j < cols_; ++j) {
+      if (i == j) matrix_[i][j] = 1;
+    }
+  }
+}
+};  // namespace s21
---
+++ 3d_viewer/affine_transformations/s21_matrix4X4.h
@@ -0,0 +1,78 @@
+#ifndef THREE_D_VIEWER_S21_MATRIX_H_
+#define THREE_D_VIEWER_S21_MATRIX_H_
+
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+
+namespace s21 {
+struct TransformData {
+  TransformData(double x_rotation = 0, double y_rotation = 0,
+                double z_rotation = 0, double shift_x = 0, double shift_y = 0,
+                double shift_z = 0, double scaler_user = 0,
+                double scaler_model = 0, bool perspective = false)
+      : x_rotation_deg(x_rotation),
+        y_rotation_deg(y_rotation),
+        z_rotation_deg(z_rotation),
+        x_shift(shift_x),
+        y_shift(shift_y),
+        z_shift(shift_z),
+        user_scaler(scaler_user),
+        model_scaler(scaler_model),
+        perspective_ortho(perspective) {}
+  // Углы поворота
+  double x_rotation_deg;
+  double y_rotation_deg;
+  double z_rotation_deg;
+  // Смещения вдоль осей
+  double x_shift;
+  double y_shift;
+  double z_shift;
+  // Коэффициент масштабирования
+  double user_scaler;
+  double model_scaler;
+  bool perspective_ortho;
+};
+
+class Matrix4X4 {
+ public:
+  Matrix4X4();
+  explicit Matrix4X4(int size);
+  Matrix4X4(const Matrix4X4& other);
+  ~Matrix4X4();
+  int GetRows() const noexcept;
+  int GetCols() const noexcept;
+  void MakeMovement();
+  void MulMatrix(const Matrix4X4& other);
+  Matrix4X4& operator=(const Matrix4X4& other) noexcept;
+  Matrix4X4& operator*=(const Matrix4X4& other) noexcept;
+  Matrix4X4& operator*=(const double& num) noexcept;
+  void MulNumber(const double& num) noexcept;
+  void SetData(const TransformData& other);
+  void print() {
+    for (int i = 0; i < 4; ++i) {
+      for (int j = 0; j < 4; ++j) {
+        std::cout << matrix_[i][j] << " ";
+      }
+      std::cout << std::endl;
+    }
+  }
+
+ private:
+  void MemoryAllocate();
+  void MemoryFree();
+  void InitializeIdentityMatrix();
+  void InitializeXRotationMatrix();
+  void InitializeYRotationMatrix();
+  void InitializeZRotationMatrix();
+  void InitializeUserScalerMatrix();
+  void InitializeModelScalerMatrix();
+  void InitializeShiftMatrix();
+  int rows_, cols_;
+  double** matrix_;
+  TransformData data_{};
+};
+};  // namespace s21
+
+#endif  // THREE_D_VIEWER_S21_MATRIX_H_
